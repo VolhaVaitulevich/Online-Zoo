5cbcf788b635c267428d7afc47797e0d99de6e6e
OlgaV <o.voytulevich@gmail.com>
2022-11-28 21:21:01 +0100

    Update range input to change content

--- pages/main/index.js
+++ pages/main/index.js
@@ -76,14 +76,15 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   });
 
-  //function to update value in Amount field
+  //update value in Amount field
   const rangeValue = () => {
     sliderValue = document.getElementById("pick-animal__randeSlider").value;
     document.getElementById("input-amount").value = document.querySelector(
       'option[data-value="' + sliderValue + '"]'
     ).value;
+    changeWidgetInfo();
   };
-
+  //update range input based in Amount field
   const rangeValueFromInput = () => {
     const options = [...rangeOptions].map((elem) => elem.value);
     if (options.includes(amountField.value)) {
@@ -91,15 +92,81 @@ document.addEventListener("DOMContentLoaded", function () {
         .querySelector('option[value="' + amountField.value + '"]')
         .getAttribute("data-value");
     }
+    sliderValue = document.getElementById("pick-animal__randeSlider").value;
+    changeWidgetInfo();
   };
-  let inputRange = document.querySelector('input[type="range"]');
-  let amountField = document.getElementById("input-amount");
-  let rangeOptions = document.querySelectorAll("datalist option");
-  //set start  value for Amount field
+
+  const changeWidgetInfo = () => {
+    switch (sliderValue) {
+      case "1":
+        widgetImage.style.backgroundImage =
+          "url(../../assets/images/animals-widget-eagle.svg)";
+        widgetTextLine1.innerHTML = "7";
+        widgetTextLine2.innerHTML = "Something about eagles.";
+        break;
+      case "2":
+        widgetImage.style.backgroundImage =
+          "url(../../assets/images/animals-widget-panda.svg)";
+        widgetTextLine1.innerHTML = "4";
+        widgetTextLine2.innerHTML = "Panda diet for the day.";
+        break;
+      case "3":
+        widgetImage.style.backgroundImage =
+          "url(../../assets/images/animals-widget-gorilla.svg)";
+        widgetTextLine1.innerHTML = "18";
+        widgetTextLine2.innerHTML = "Something about gorillas.";
+        break;
+      case "4":
+        widgetImage.style.backgroundImage =
+          "url(../../assets/images/animals-widget-panda.svg)";
+        widgetTextLine1.innerHTML = "1";
+        widgetTextLine2.innerHTML = "Something about pandas.";
+        break;
+      case "5":
+        widgetImage.style.backgroundImage =
+          "url(../../assets/images/animals-widget-alligator.svg)";
+        widgetTextLine1.innerHTML = "3";
+        widgetTextLine2.innerHTML = "Something about alligators.";
+        break;
+      case "6":
+        widgetImage.style.backgroundImage =
+          "url(../../assets/images/animals-widget-eagle.svg)";
+        widgetTextLine1.innerHTML = "12";
+        widgetTextLine2.innerHTML = "More info about eagles.";
+        break;
+      case "7":
+        widgetImage.style.backgroundImage =
+          "url(../../assets/images/animals-widget-alligator.svg)";
+        widgetTextLine1.innerHTML = "2";
+        widgetTextLine2.innerHTML = "More info about alligators.";
+        break;
+      case "8":
+        widgetImage.style.backgroundImage =
+          "url(../../assets/images/animals-widget-gorilla.svg)";
+        widgetTextLine1.innerHTML = "1";
+        widgetTextLine2.innerHTML = "More info about gorillas.";
+        break;
+      default:
+        widgetImage.style.backgroundImage =
+          "url(../../assets/images/animals-widget-panda.svg)";
+        widgetTextLine1.innerHTML = "4";
+        widgetTextLine2.innerHTML = "More info about pandas.";
+    }
+  };
+
+  const inputRange = document.querySelector('input[type="range"]');
+  const amountField = document.getElementById("input-amount");
+  const rangeOptions = document.querySelectorAll("datalist option");
+
+  //show cotent depend on sliderValue
+  const widgetImage = document.querySelector(".animals-widget__image");
+  const widgetTextLine1 = document.querySelector(".animal-widget__info-line1");
+  const widgetTextLine2 = document.querySelector(".animal-widget__info-line2");
+
+  //set start value for Amount field
   let sliderValue = document.getElementById("pick-animal__randeSlider").value;
-  document.getElementById("input-amount").value = document.querySelector(
-    'option[data-value="' + sliderValue + '"]'
-  ).value;
+  rangeValue();
+
   inputRange.addEventListener("input", rangeValue);
   amountField.addEventListener("input", rangeValueFromInput);
 });
